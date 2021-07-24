3dbf1c9ceec6dd4adf948be98f1dbf11bf084270
SRC0747 <srcsrk1998@gmail.com>
2021-07-24 21:17:03 +0530

    calculating wage till total working days is reached for a month.

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,14 +5,18 @@ isFullTime=2
 randomCheck=$((RANDOM%3))
 empWagePerHr=20
 numWorkingDays=20
-case $empCheck in
-	$isFullTime)	echo "Employee is Full-time Present."
-			empHrs=8
-			;;
-	$isPartTime)	echo "Employee is Part time present."
-			empHrs=4
-			;;
-	*)		echo "Empployee is Absent."
-			empHrs=0
-esac
-wage=$(($empWagePerHr*$empHrs))
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	case $empCheck in
+		$isFullTime)	echo "Employee is Full-time Present."
+				empHrs=8
+				;;
+		$isPartTime)	echo "Employee is Part time present."
+				empHrs=4
+				;;
+		*)		echo "Empployee is Absent."
+				empHrs=0
+				;;
+	esac
+	wage=$(($empWagePerHr*$empHrs))
+done
