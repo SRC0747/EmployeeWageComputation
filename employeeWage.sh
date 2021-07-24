7b962b1b414dba0f7d203c4c08a28fbd9a26c55a
SRC0747 <srcsrk1998@gmail.com>
2021-07-24 19:35:19 +0530

    Checkng Employee is Present or absent.

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation problem."
+isCheck=1
+randomCheck=$((RANDOM%2))
+if [ $isCheck  -eq  $randomCheck ]
+then
+	echo "Employee is Present."
+else
+	echo "Employee is absent."
+fi
